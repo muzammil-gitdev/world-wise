dcdf87f4fa466ef0365526e8e737a8fa22bf5d46
muzammil-gitdev <muzammil.uiit@gmail.com>
2026-02-16 16:06:52 +0500

    added pages and componenets

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,12 +1,7 @@
-import PageNav from "../components/PageNav";
-
-function PageNotFound() {
+export default function PageNotFound() {
   return (
     <div>
-      <PageNav />
-      <div>Not Found:(</div>
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
-
-export default PageNotFound;
