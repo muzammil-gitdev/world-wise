022be55ec3ea610bc1dec659caaaac18096cf2ab
muzammil-gitdev <muzammil.uiit@gmail.com>
2026-02-17 16:02:55 +0500

    added nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,11 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
-          <Route path="app" element={<AppLayout />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route path="cities" element={<p>List of Cities</p>} />
+            <Route path="countries" element={<p>List of Countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,13 @@
 import AppNav from "../components/AppNav";
+import Map from "../components/Map";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
 
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      Applyaout
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
